922e8e7614d4f40059e1c582cb599045d854a5f3
srisai-cherukuri <cherukuri.srisai@outlook.com>
2026-01-22 00:05:17 -0600

    genvar

---
+++ Hierarchical Modeling/adder_multi_bit.v
@@ -0,0 +1,28 @@
+`include "full_adder_gate.v"
+module adder#(
+    parameter WIDTH = 6
+)(
+    input [WIDTH-1:0] A,
+    input [WIDTH-1:0] B,
+    input Cin,
+    output [WIDTH-1:0] Sum,
+    output Cout
+);
+
+wire [WIDTH:0] carry;
+
+genvar i;
+assign carry[0] = Cin;
+generate
+    for(i=0; i<WIDTH; i=i+1) begin : adder_loop
+        full_adder FA (
+            .a(A[i]),
+            .b(B[i]),
+            .ci(carry[i]),
+            .s(Sum[i]),
+            .co(carry[i+1])
+        );
+    end
+endgenerate
+assign Cout = carry[WIDTH];
+endmodule
--- Hierarchical Modeling/tb_full_adder.v
+++ Hierarchical Modeling/tb_full_adder.v
@@ -1,14 +1,15 @@
-`include "full_adder_4bit_dataflow.v"
+`include "adder_multi_bit.v"
 
 module tb;
 
-reg [3:0] a, b;
+parameter WIDTH = 6;
+reg [WIDTH-1:0] a, b;
 reg cin;
-wire [3:0] s;
+wire [WIDTH-1:0] s;
 wire co;
 integer seed;
 
-full_adder_4bit dut(.A(a),.B(b),.Cin(cin),.Sum(s),.Cout(co));
+adder dut(.A(a),.B(b),.Cin(cin),.Sum(s),.Cout(co));
 
 initial begin
    
@@ -16,7 +17,7 @@ initial begin
    repeat(15) begin
     {a,b,cin} = $random(seed);
     #1;
-    $display("%0t - a = %b, b = %b, ci = %b, sum = %b, carryout = %b", $time, a, b, cin, s, co);
+    $display("%0t - a = %d, b = %d, ci = %b, sum = %d, carryout = %b, total = %d", $time, a, b, cin, s, co, {co, s});
    
    end 
 end
